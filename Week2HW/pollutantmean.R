47bf30eedca15f3b47bc8150a897fa5031556a81
AMPrescott <bcamprescott@gmail.com>
2015-06-09 23:40:42 -0400

    This corresponds to initial debug and allows for execution

--- Week2HW/pollutantmean.R
+++ Week2HW/pollutantmean.R
@@ -2,29 +2,32 @@
 #referenced id and averaging over available values for the specified pollutant.  
 #The files are located in the location indicated by directory
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+  #Initialize vector for storing file names
+  fileID <- c(rep('x',length(id)))
+  
   #If values listed in vector id are less than 100, add leading zeros to match 
   #file name formats and concatonate with directory.  Open and merge the files in a 
   #single data frame
-  for(site in id){
+  for(site in seq_len(length(id))){
     
     if(id[site]<10){
-      fileID[site] <-  paste0(directory,"\00",id,".csv")
+      fileID[site] <-  paste0(directory,"00",id[site],".csv")
     }
     else if(id[site]<100){
-      fileID[site] <-  paste0(directory,"\0",id,".csv")
+      fileID[site] <-  paste0(directory,"0",id[site],".csv")
     }
     else{
-      fileID[site] <-  paste0(directory,id,".csv")
+      fileID[site] <-  paste0(directory,id[site],".csv")
     }
     
     #Open first file into allSitesData data frame
     if(site == 1){
-      allSitesData <- read.csv(fileID[site])
+      allSitesData <- data.frame(read.csv(fileID[site]))
     }
     #If first file is already read, read file into temporary data frame and merge with allSitesData
     else{
-      tempData <- read.csv(fileID[site])
-      allSitesData <- merge(allSitesData, tempData)
+      tempData <- data.frame(read.csv(fileID[site]))
+      allSitesDate <- merge(allSitesData, tempData, all = TRUE)
     }
   }
   
