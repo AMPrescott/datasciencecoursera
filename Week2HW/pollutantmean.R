7e28de88aed701ee48b3fae1ddd758bcebabbe6a
AMPrescott <bcamprescott@gmail.com>
2015-06-10 20:35:17 -0400

    Completed troublshooting function pollutantmean.R

--- Week2HW/pollutantmean.R
+++ Week2HW/pollutantmean.R
@@ -11,27 +11,29 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   for(site in seq_len(length(id))){
     
     if(id[site]<10){
-      fileID[site] <-  paste0(directory,"00",id[site],".csv")
+      fileID[site] <-  paste0(directory,"/00",id[site],".csv")
     }
     else if(id[site]<100){
-      fileID[site] <-  paste0(directory,"0",id[site],".csv")
+      fileID[site] <-  paste0(directory,"/0",id[site],".csv")
     }
     else{
-      fileID[site] <-  paste0(directory,id[site],".csv")
+      fileID[site] <-  paste0(directory,"/",id[site],".csv")
     }
     
     #Open first file into allSitesData data frame
     if(site == 1){
-      allSitesData <- data.frame(read.csv(fileID[site]))
+      allSitesData <- data.matrix(read.csv(fileID[site]))
     }
     #If first file is already read, read file into temporary data frame and merge with allSitesData
     else{
-      tempData <- data.frame(read.csv(fileID[site]))
-      allSitesDate <- merge(allSitesData, tempData, all = TRUE)
+      tempData <- data.matrix(read.csv(fileID[site]))
+      allSitesData <-rbind(allSitesData,tempData)
     }
   }
   
   #Calculate the combined site mean of the specified pollutant column
-  comSiteMean <- mean(allSitesData[[pollutant]], na.rm = TRUE)
+  comSiteMean <- mean(allSitesData[,pollutant], na.rm = TRUE)
+  
+  print(comSiteMean)
   
 }
